de466d6232315f8e29897145ca93d8c16a116213
Adam Żaczek <adam.zaczek@netguru.pl>
2017-04-22 13:45:56 +0200

    Add Relay Plural Note In First Test Query

--- client/components/Index/IndexContainer.js
+++ client/components/Index/IndexContainer.js
@@ -1,10 +1,11 @@
 import Relay from 'react-relay';
 import IndexComponent from './IndexComponent';
 
+// need to check how this plural works
 export default Relay.createContainer(IndexComponent, {
   fragments: {
     users: () => Relay.QL`
-      fragment on User {
+      fragment on User @relay(plural: true) {
         email,
       }
     `,
