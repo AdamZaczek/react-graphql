b0815db5ca219dd324fa921becc8588658b20169
Adam Żaczek <adam.zaczek@netguru.pl>
2017-04-28 22:50:57 +0200

    Remove Routes, Add Relay Modern Plugin In .babelrc

--- client/index.js
+++ client/index.js
@@ -5,13 +5,18 @@ import { browserHistory, applyRouterMiddleware, Router } from 'react-router';
 import useRelay from 'react-router-relay';
 
 // import Route from './routes/Route';
-import UsersRoute from './routes/StoriesQuery';
+// import UsersRoute from './routes/StoriesQuery';
 
 
 const rootNode = document.createElement('div');
 document.body.appendChild(rootNode);
 
+
+// ReactDOM.render(
+//   <Router history={browserHistory} routes={UsersRoute} render={applyRouterMiddleware(useRelay)} environment={Relay.Store} />,
+//   rootNode
+// );
 ReactDOM.render(
-  <Router history={browserHistory} routes={UsersRoute} render={applyRouterMiddleware(useRelay)} environment={Relay.Store} />,
+  <Router history={browserHistory} render={applyRouterMiddleware(useRelay)} environment={Relay.Store} />,
   rootNode
 );
