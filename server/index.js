e1af512b1e4b1c89724fa24022d9dd2a1bedaffc
Adam Żaczek <adam.zaczek@netguru.pl>
2017-05-01 12:51:41 +0200

    Remove Some Unused Deps

--- server/index.js
+++ server/index.js
@@ -1,14 +1,11 @@
 /* eslint-disable no-console, no-shadow */
 import path from 'path';
-import webpack from 'webpack';
 import express from 'express';
 import graphQLHTTP from 'express-graphql';
-import WebpackDevServer from 'webpack-dev-server';
 import historyApiFallback from 'connect-history-api-fallback';
 import chalk from 'chalk';
 import bodyParser from 'body-parser';
 import passport from 'passport';
-import webpackConfig from '../webpack.config';
 import { config, auth } from './config/environment/';
 import schema from './appData/schema';
 import myMongoCredentials from './myMongoCredentials';
@@ -57,31 +54,14 @@ if (config.env === 'development') {
   }));
   graphql.listen(config.graphql.port, () => console.log(chalk.green(`GraphQL is listening on port ${config.graphql.port}`)));
   graphql.use(bodyParser.json({ type: '*/*' }));
-
-  // Launch Relay by using webpack.config.js
-  // const relayServer = new WebpackDevServer(webpack(webpackConfig), {
-  //   contentBase: '/build/',
-  //   proxy: {
-  //     '/graphql': `http://localhost:${config.graphql.port}`
-  //   },
-  //   stats: {
-  //     colors: true
-  //   },
-  //   hot: true,
-  //   historyApiFallback: true
-  // });
-
-  // Serve static resources
-  // relayServer.use('/', express.static(path.join(__dirname, '../build')));
-  // relayServer.listen(config.port, () => console.log(chalk.green(`Relay is listening on port ${config.port}`)));
 } else if (config.env === 'production') {
   // Launch Relay by creating a normal express server
-  const relayServer = express();
+  const productionServer = express();
 
-  relayServer.get('/login/facebook',
+  productionServer.get('/login/facebook',
     passport.authenticate('facebook', { scope: ['email', 'user_location'], session: false }),
   );
-  relayServer.get('/login/facebook/return',
+  productionServer.get('/login/facebook/return',
     passport.authenticate('facebook', { failureRedirect: '/login', session: false }),
     (req, res) => {
       const expiresIn = 60 * 60 * 24 * 180; // 180 days
@@ -91,9 +71,9 @@ if (config.env === 'development') {
     },
   );
 
-  relayServer.use(historyApiFallback());
-  relayServer.use('/', express.static(path.join(__dirname, '../build')));
-  relayServer.use('/graphql', graphQLHTTP({ schema }));
-  relayServer.use(bodyParser.json({ type: '*/*' }));
-  relayServer.listen(config.port, () => console.log(chalk.green(`Relay is listening on port ${config.port}`)));
+  productionServer.use(historyApiFallback());
+  productionServer.use('/', express.static(path.join(__dirname, '../build')));
+  productionServer.use('/graphql', graphQLHTTP({ schema }));
+  productionServer.use(bodyParser.json({ type: '*/*' }));
+  productionServer.listen(config.port, () => console.log(chalk.green(`App is listening on port ${config.port}`)));
 }
