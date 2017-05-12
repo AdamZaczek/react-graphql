bc78128987ea123000492c0b2a619e4891437ba0
Adam Żaczek <adam.zaczek@netguru.pl>
2017-05-13 00:12:53 +0200

    Add Working UserClaim Model

--- server/appData/models/UserClaim.js
+++ server/appData/models/UserClaim.js
@@ -1,25 +1,8 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
+import mongoose from 'mongoose';
 
-import DataType from 'sequelize';
-import Model from '../sequelize';
-
-const UserClaim = Model.define('UserClaim', {
-
-  type: {
-    type: DataType.STRING,
-  },
-
-  value: {
-    type: DataType.STRING,
-  },
-
-});
+const UserClaim = mongoose.model('UserClaim', ({
+  type: { type: String },
+  value: { type: String },
+}));
 
 export default UserClaim;
