4f762a18271061b6e8410d52de9f54fa3cbd0f22
Adam Żaczek <adam.zaczek@netguru.pl>
2017-05-13 00:16:08 +0200

    Update UserProfile

--- server/appData/models/UserLogin.js
+++ server/appData/models/UserLogin.js
@@ -1,27 +1,8 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
+import mongoose from 'mongoose';
 
-import DataType from 'sequelize';
-import Model from '../sequelize';
-
-const UserLogin = Model.define('UserLogin', {
-
-  name: {
-    type: DataType.STRING(50),
-    primaryKey: true,
-  },
-
-  key: {
-    type: DataType.STRING(100),
-    primaryKey: true,
-  },
-
-});
+const UserLogin = mongoose.model('UserLogin', ({
+  name: { type: String, primaryKey: true},
+  key: { type: String },
+}));
 
 export default UserLogin;
